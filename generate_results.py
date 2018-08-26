6e3692ad89026c9fccd7c11653800bc2c80f1b57
scmanjarrez <serchiman@gmail.com>
2018-08-26 05:38:59 +0200

    Added substitution variables to results

--- generate_results.py
+++ generate_results.py
@@ -49,7 +49,7 @@ pattern = re.compile('(\w+\.bin)\s*(\d+)\s*/\s*(\d+)')
 with open(sys.argv[4], 'r') as f:
     for line in f.read().splitlines():
         elem, sub_done, tot_sub = pattern.match(line).groups()
-        sub_rdy[elem] = (sub_done, tot_sub)
+        sub_rdy[elem] = (int(sub_done), int(tot_sub))
 
 rdn = {}
 with open(sys.argv[3], 'r') as f:
@@ -61,7 +61,7 @@ with open(sys.argv[3], 'r') as f:
 
 results = open(sys.argv[5], 'w')
 
-results.write('{:50s}{:10s}{:10s}{:10s}\n\n'.format('Sample_MD5', 'Original', 'Random_Y', 'Random_N'))
+results.write('{:50s}{:10s}{:10s}{:10s}{:15s}{:15s}\n\n'.format('Sample_MD5', 'Original', 'Random_Y', 'Random_N', 'Sust_Rnd_Y', 'Sust_Rnd_N'))
 
 to_plot = []
 sup_rdy_org = []
@@ -70,15 +70,16 @@ sup_rdn_org = []
 inf_rdn_org = []
 
 for idx, elem in enumerate(sorted(org, key=org.get)):
-    results.write('{:50s}{:5.2f}{:10.2f}{:10.2f}\n'.format(elem, org[elem], rdy[elem], rdn[elem]))
-    to_plot.append((org[elem], rdy[elem], rdn[elem]))
+    n_sub, t_sub = sub_rdy[elem]
+    results.write('{:50s}{:5.2f}{:10.2f}{:10.2f}{:15.0f}{:15.0f}\n'.format(elem, org[elem], rdy[elem], rdn[elem], n_sub, t_sub))
+    to_plot.append((org[elem], rdy[elem], rdn[elem], n_sub, t_sub))
     if rdy[elem] > org[elem]:
         sup_rdy_org.append(str(idx+1))
-    elif org[elem] - rdy[elem] <= 5.0:
+    elif org[elem] - rdy[elem] <= 2.0:
         inf_rdy_org.append(str(idx+1))
     if rdn[elem] > org[elem]:
         sup_rdn_org.append(str(idx+1))
-    elif org[elem] - rdn[elem] <= 5.0:
+    elif org[elem] - rdn[elem] <= 2.0:
         inf_rdn_org.append(str(idx+1))
 
 to_plot = sorted(to_plot, key=lambda x: x[0])
@@ -86,11 +87,13 @@ to_plot2 = sorted(to_plot, key=lambda x: x[1])
 to_plot3 = sorted(to_plot, key=lambda x: x[2])
 
 plot = np.array(to_plot)
+#aux = np.amax(plot[:, 3])
+#aux1 = np.amin(plot[:, 3])
+#plot[:, 3] = plot[:, 3] / abs(aux) * 10.
 plot2 = np.array(to_plot2)
 plot3 = np.array(to_plot3) 
 
 plt_sz = len(to_plot)
-
 org_mean = np.mean(plot[:, 0])
 rdy_mean = np.mean(plot[:, 1])
 rdn_mean = np.mean(plot[:, 2]) 
@@ -98,18 +101,18 @@ rdn_mean = np.mean(plot[:, 2])
 results.write('\n{:50s}{:5.2f}{:10.2f}{:10.2f}\n'.format("Medias", org_mean, rdy_mean, rdn_mean))
 
 results.write('\n{:50s}{:50s}\n'.format("Sim. Sup. rdy_org", ", ".join(sup_rdy_org)))
-results.write('{:50s}{:50s}\n'.format("Sim. Inf. rdy_org", ", ".join(inf_rdy_org)))
+results.write('{:50s}{:50s}\n'.format("Sim. Inf(2). rdy_org", ", ".join(inf_rdy_org)))
 results.write('\n{:50s}{:50s}\n'.format("Sim. Sup. rdn_org", ", ".join(sup_rdn_org)))
-results.write('{:50s}{:50s}\n'.format("Sim. Inf. rdn_org", ", ".join(inf_rdn_org)))
+results.write('{:50s}{:50s}\n'.format("Sim. Inf(2). rdn_org", ", ".join(inf_rdn_org)))
 
 results.close()
 
 print("Media org: {}\nMedia rdy: {}\nMedia rdn: {}".format(org_mean, rdy_mean, rdn_mean))
 
 print("[rdy] Sim. Sup. rdy_org: {}".format(", ".join(sup_rdy_org)))
-print("[rdy] Sim. Inf(<=5). rdy_org: {}".format(", ".join(inf_rdy_org)))
+print("[rdy] Sim. Inf(2). rdy_org: {}".format(", ".join(inf_rdy_org)))
 print("[rdn] Sim. Sup. rdn_org: {}".format(", ".join(sup_rdn_org)))
-print("[rdn] Sim. Inf(<=5). rdn_org: {}".format(", ".join(inf_rdn_org)))
+print("[rdn] Sim. Inf(2). rdn_org: {}".format(", ".join(inf_rdn_org)))
 
 rang = range(1, plt_sz + 1)
 
@@ -144,10 +147,24 @@ ax2.plot(list(map(int, inf_rdy_org)), plot[list(map(lambda x: x-1, map(int, inf_
 ax2.plot(list(map(int, sup_rdn_org)), plot[list(map(lambda x: x-1, map(int, sup_rdn_org))), 2], 'darkgreen', marker=7, label='Sup. Rdn. Org', linestyle='None')
 ax2.plot(list(map(int, inf_rdn_org)), plot[list(map(lambda x: x-1, map(int, inf_rdn_org))), 2], 'olive', marker=6, label='Inf. Rdn. Org', linestyle='None')
 # Modificar etiqueta de ejes y su escala
-ax2.set_xlabel("Muestras")
+ax2.set_xlabel(u"Muestras")
 ax2.set_ylabel(u"Índice de detección (%)")
 ax2.set_xticks(range(0, plt_sz + 10, 100))
 ax2.axis([0, 1510, 0, 100])
+
+#plt3 = plt.figure(3)
+#ax3 = plt3.add_subplot(1,1,1)
+#yaxis2 = ax3.twinx()
+# Muestras vs índice detección
+#ax3.plot(rang, plot[:, 0], 'k', label='Original', linewidth=1)
+#ax3.plot(rang, plot[:, 1], 'r', label='Random_Y', linewidth=0.5)
+#ax3.plot(rang, plot[:, 2], 'lime', label='Random_N', linewidth=1.2, alpha=0.6)
+# Nuevo eje Y
+#yaxis2.set_ylabel("Sustituciones")
+#yaxis2.plot(rang, plot[:, 3], 'magenta', marker='.', label='Sust. Hechas.', linestyle='None')
+#yaxis2.plot(rang, plot[:, 4], 'blue', marker='.', label='Sust. Tot.', linestyle='None')
+#yaxis2.set_ylim(0, 4)
+#yaxis2.set_yticks(range(0, 25, 1))
 
 plt.legend()
 plt.show()
